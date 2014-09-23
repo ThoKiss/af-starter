a6e5b54c67b48ccf2f5a6b6cfa21a0a0128cdfe0
ThoKiss <Thorsten.Kiss81@gmail.com>
2014-09-23 12:35:21 +0200

    Angular Fullstack Starter

---
+++ client/components/navbar/navbar.controller.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('boilerplateApp')
+  .controller('NavbarCtrl', function ($scope, $location) {
+    $scope.menu = [{
+      'title': 'Home',
+      'link': '/'
+    }];
+
+    $scope.isCollapsed = true;
+
+    $scope.isActive = function(route) {
+      return route === $location.path();
+    };
+  });
